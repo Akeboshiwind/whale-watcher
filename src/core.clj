4719d3740f195eea45081872e2462eaf2790d544
Oliver Marshall <olivershawmarshall@gmail.com>
2023-10-16 22:11:54 +0100

    refactor: extract container-updates & switch to log instead of println

--- src/core.clj
+++ src/core.clj
@@ -4,7 +4,8 @@
     [regctl :as r]
     [semver :refer [semver? semver compare-semver >semver]]
     [telegram :as t]
-    [clojure.string :as str]))
+    [clojure.string :as str]
+    [clojure.tools.logging :as log]))
 
 (defn name&tag [full-name]
   (let [[name tag] (str/split full-name #":")]
@@ -35,43 +36,47 @@
          (str/join "\n"
                    (map format-update updates)))))
 
+(defn sift
+  "Returns a vector of [(filter pred coll) (remove pred coll)]"
+  [pred coll]
+  (let [g (group-by (comp boolean pred) coll)]
+    [(g true) (g false)]))
+
+(defn container-updates []
+  (let [[images ignored] (->> (d/ps) (sift (complement ignore?)))
+
+        _ (log/info (str "Skipping due to ignore label: "
+                         (->> ignored (map :image) (str/join ", "))))
+
+        [images skipped] (->> images
+                              (map (comp name&tag :image))
+                              (sift :tag))
+
+        _ (log/info (str "Skipping due to missing/filtered tag: "
+                         (->> skipped (map :name) (str/join ", "))))
+
+        updates (for [{:keys [name tag]} images]
+                  (do
+                    (log/info (str "Checking: " name ":" tag))
+                    (let [latest-tag (latest-tag name)]
+                      (when (>semver (semver latest-tag) (semver tag))
+                        {:name name :tag tag :latest-tag latest-tag}))))]
+    (->> updates (remove nil?))))
+
 (defn -main [& _args]
-  (println "Checking for updates")
+  (log/info "Checking for updates")
   (let [token (System/getenv "TELEGRAM_TOKEN")
         chat-id (System/getenv "TELEGRAM_CHAT_ID")]
     (try
-      (let [images (d/ps)
-
-            _ (apply println "Skipping due to ignore label:"
-                     (->> images
-                          (filter ignore?)
-                          (map :image)))
-
-            images (->> images
-                        (remove ignore?)
-                        (map (comp name&tag :image)))
-
-            _ (apply println "Skipping due to missing/filtered tag:"
-                     (->> images
-                          (remove :tag)
-                          (map :name)))
-
-            images (->> images (filter :tag))
-
-            updates (for [{:keys [name tag]} images]
-                      (do
-                        (println (str "Checking: " name ":" tag))
-                        (let [latest-tag (latest-tag name)]
-                          (when (>semver (semver latest-tag) (semver tag))
-                            {:name name :tag tag :latest-tag latest-tag}))))
-            updates (->> updates (remove nil?))]
+      (let [updates (container-updates)]
         (when (seq updates)
           (t/send-message token
                           {:chat_id chat-id
                            :parse_mode "MarkdownV2"
                            :text (format-message updates)})))
-      (catch Exception _
+      (catch Exception e
+        (log/error e "Error checking for updates")
         (t/send-message token
                         {:chat_id chat-id
                          :text "Error checking for updates"}))))
-  (println "Done!"))
+  (log/info "Done!"))
